4d96923c9720344743f46da82bd9d9e6bbe80ca2
mrleo1994 <thienchipham.st@gmail.com>
2024-02-23 12:18:05 +0700

    arithmectic operators end lesion 2

---
+++ lession_2/index.js
@@ -0,0 +1,50 @@
+/**
+ * 1. Toán tử số học - Arithmetic
+ * 2. Toán tử gán - Assignment
+ * 3. Toán tử so sánh - Comparison
+ * 4. Toán tử logic - Logical
+ */
+
+// var a = 1 + 2; // phép cộng là toán tử số học
+// console.log(a);
+// var fullName = "Pham Chi Thien"; // phép bằng là toán tử gán
+
+// var b = 1;
+// var c = 2;
+
+// if (b == c) {
+//   // phép "==" (hoặc "<", ">") là toán tử so sánh
+//   alert("Dung");
+// }
+
+// Toán tử số
+var a = 8;
+var b = 3;
+var c = a % b;
+console.log(c);
+// Toán tử làm hậu tố và tiền tố
+// var number1 = 1;
+
+// console.log(number1++); // 1
+// console.log(number1); // 2
+
+// console.log(number1++); // 2
+// console.log(number1); // 3
+
+// var number2 = 1;
+
+// console.log(++number2); // 2
+// console.log(number2); // 2
+
+// console.log(++number2); // 3
+// console.log(number2); // 3
+
+/**
+ * Toán tử logic
+ * 1. && - And
+ * 2. || - Or
+ * 3. ! - Not
+ */
+
+var d = typeof b;
+console.log(typeof d);
