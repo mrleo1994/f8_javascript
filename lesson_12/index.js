b170d89928dd08e930b1704906cc87794c2049e4
mrleo1994 <thienchipham.st@gmail.com>
2024-03-02 23:25:30 +0700

    lesson 12 element method

---
+++ lesson_12/index.js
@@ -0,0 +1,16 @@
+/**
+ * HTML DOM
+ *
+ * 1. element: ID, class, tag, CSS selector, HTML collection
+ * ID: .getElementById()
+ * class: .getElementsByClassName()
+ * tag: .getElementByTagName('h1')
+ * css selector: .querySelector('.box .heading-2:nth-child(3)')
+ * css selector: .querySelectorAll('.box .heading-2')
+ * html collection: ít sử dụng => document.forms['form-1']
+ *
+ * 2. attribute
+ * 3. text
+ *
+ *
+ */
