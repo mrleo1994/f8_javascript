7800b7eb021ce5f7b4407e93a7b2a955c426d818
mrleo1994 <thienchipham.st@gmail.com>
2024-03-05 22:27:13 +0700

    lesson 12 DOM end

--- lesson_12/index.js
+++ lesson_12/index.js
@@ -41,3 +41,32 @@
 //   }
 // }
 // render(courses);
+
+/**
+ * DOM event
+ *
+ *  1. Attribute events
+ *  2. Assign event using the element node
+ */
+document.querySelector("button").onclick = (e) =>
+  (e.target.style.color = "#fff");
+
+/**
+ *    1. preventDefault
+ *    2. stopPropagation
+ */
+
+/**
+ *    1. Event listener
+ *    2. JSON
+ *    3. Fetch
+ *    4. DOM location
+ *    5. Local storage
+ *    6. Session storage
+ *    7. Coding convention
+ *    8. Best Practices
+ *    9. Mistakes
+ *    10. Performance
+ *
+ *
+ */
