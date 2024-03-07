cbd15742d0dff1986b67e960c0a4d393186e594e
mrleo1994 <thienchipham.st@gmail.com>
2024-03-08 00:13:05 +0700

    lesson 13 JSON

---
+++ lesson_13/index.js
@@ -0,0 +1,11 @@
+/**
+ * JSON
+ * 1. Là một định dạng dữ liệu
+ * 2. JavaScript Object Notation
+ * 3. JSON: Number, Boolean, Null, Array, Object, String
+ * Mã hóa: JSON.stringify()
+ * Giải mã: JSON.parse()
+ */
+
+var a = "1";
+console.log(JSON.parse(a));
