1e654fe8d280e4945cb8e0ea2dda332f4ba2db22
mrleo1994 <thienchipham.st@gmail.com>
2024-03-14 23:05:24 +0700

    lesson 14 ES6

---
+++ lesson_14/index.js
@@ -0,0 +1,17 @@
+/**
+ * 1. Let, const
+ * 2. Template Literals
+ * 3. Multi-line String
+ * 4. Arrow Function
+ * 5. Classes
+ * 6. Default Parameter
+ * 7. Destructuring
+ * 8. Rest Parameter
+ * 9. Spread Operator
+ * 10. Enhanced Object Literals
+ * 11. Tagged Template Literals
+ * 12. Modules
+ */
+
+// 1. Var/ Let, Const: Scope, Hosting
+// 2. Const/ Var, Let: Assignment
